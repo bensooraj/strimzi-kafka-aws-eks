232f62a044b7de33a8779f5f3730d2e283c08715
Ben Sooraj <bensoorajm@gmail.com>
2019-10-04 01:56:56 +0530

    Consumer changes

--- test/testKafkaConsumer/kafkaConsumer.js
+++ test/testKafkaConsumer/kafkaConsumer.js
@@ -1,22 +1,27 @@
-require('dotenv').config({
-    path: __dirname + '../../test/.env'
-})
-
+if (
+    process.env.MODE === "dev"
+) {
+    require('dotenv').config({
+        path: '../../test/.env'
+    })
+}
 const { Kafka } = require('kafkajs');
 const kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: [`${process.env.KAFKA_BROKER_1}:9092`, `${process.env.KAFKA_BROKER_2}:9092`, `${process.env.KAFKA_BROKER_3}:9092`],
-    // authenticationTimeout: 10000,
-    // connectionTimeout: 10000,
-    // sasl: {
-    //     mechanism: 'scram-sha-256',
-    //     username: process.env.KAFKA_SASL_USERNAME,
-    //     password: process.env.KAFKA_SASL_PASSWORD
-    // }
+    clientId: 'strimzi-node-consumer',
+    brokers: [`${process.env.KAFKA_BROKER_1}:9094`, `${process.env.KAFKA_BROKER_2}:9094`, `${process.env.KAFKA_BROKER_3}:9094`],
+    authenticationTimeout: 10000,
+    connectionTimeout: 10000,
+    sasl: {
+        mechanism: 'scram-sha-256',
+        username: process.env.KAFKA_SASL_USERNAME,
+        password: process.env.KAFKA_SASL_PASSWORD
+    },
+    ssl: true
 });
-
 const topic = process.env.KAFKA_TOPIC_NAME;
-const consumer = kafka.consumer({ groupId: 'test-group' });
+const consumer = kafka.consumer({ groupId: process.env.KAFKA_CONSUMER_GROUP_ID });
+
+const messageHandler = require("./kafkaMessageHandler");
 
 const run = async () => {
     await consumer.connect()
@@ -26,8 +31,15 @@ const run = async () => {
         //   console.log(batch)
         // },
         eachMessage: async ({ topic, partition, message }) => {
-            const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
-            console.log(`- ${prefix} ${message.key}#${message.value}`)
+            // const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+            // console.log(`- ${prefix} ${message.key}#${message.value}`)
+
+            const messageValue = JSON.parse(message.value);
+            try {
+                await messageHandler(messageValue.type, messageValue.action, messageValue.params);
+            } catch (error) {
+                console.log("[Kafka Message Processing] Error: ", error);
+            }
         },
     })
 }
@@ -59,75 +71,3 @@ signalTraps.map(type => {
         }
     })
 })
-// const kafkaLogging = require('kafka-node/logging');
-// kafkaLogging.setLoggerProvider(() => {
-//     return {
-//         debug: console.debug.bind(console),
-//         info: console.info.bind(console),
-//         warn: console.warn.bind(console),
-//         error: console.error.bind(console)
-//     };
-// });
-
-// const messageHandler = require("./kafkaMessageHandler");
-// const brokerConnection = `${process.env.KAFKA_BROKER_1}:9092,${process.env.KAFKA_BROKER_2}:9092,${process.env.KAFKA_BROKER_3}:9092`;
-// console.log("brokerConnection: ", brokerConnection);
-
-// const kafka = require('kafka-node'),
-
-//     ConsumerGroup = kafka.ConsumerGroup,
-
-//     consumerGroupOptions = {
-//         kafkaHost: brokerConnection, // connect directly to kafka broker (instantiates a KafkaClient)
-//         // batch: undefined, // put client batch settings if you need them
-//         // ssl: true, // optional (defaults to false) or tls options hash
-//         groupId: process.env.KAFKA_CONSUMER_GROUP_ID,
-//         sessionTimeout: 15000,
-//         // An array of partition assignment protocols ordered by preference.
-//         // 'roundrobin' or 'range' string for built ins (see below to pass in custom assignment protocol)
-//         protocol: ['roundrobin'],
-//         encoding: 'utf8', // default is utf8, use 'buffer' for binary data
-
-//         fetchMaxBytes: 10 * 1024 * 1024, // 10 MB
-
-//         // Offsets to use for new groups other options could be 'earliest' or 'none' (none will emit an error if no offsets were saved)
-//         // equivalent to Java client's auto.offset.reset
-//         fromOffset: 'latest', // default
-//         // commitOffsetsOnFirstJoin: true, // on the very first time this consumer group subscribes to a topic, record the offset returned in fromOffset (latest/earliest)
-//         // how to recover from OutOfRangeOffset error (where save offset is past server retention) accepts same value as fromOffset
-//         outOfRangeOffset: 'earliest', // default
-//         autoCommit: true,
-//         autoCommitIntervalMs: 5000
-//         // Callback to allow consumers with autoCommit false a chance to commit before a rebalance finishes
-//         // isAlreadyMember will be false on the first connection, and true on rebalances triggered after that
-//         // onRebalance: (isAlreadyMember, callback) => { callback(); } // or null
-//     },
-
-//     consumerGroup = new ConsumerGroup(consumerGroupOptions, process.env.KAFKA_TOPIC_NAME);
-
-// consumerGroup.on('connect', () => {
-//     console.log("Kafka Consumer Group Connected");
-// });
-
-// consumerGroup.on('error', (error) => {
-//     console.log("Kafka Consumer Group Error: ", error);
-// });
-
-// consumerGroup.on('rebalancing', () => {
-//     console.log("Kafka Consumer Group Rebalancing");
-// });
-
-// consumerGroup.on('rebalanced', () => {
-//     console.log("Kafka Consumer Group Rebalanced");
-// });
-
-// consumerGroup.on('message', async (message) => {
-//     // console.log("Message Received: ", message);
-
-//     const messageValue = JSON.parse(message.value);
-//     try {
-//         await messageHandler(messageValue.type, messageValue.action, messageValue.params);
-//     } catch (error) {
-//         console.log("[Kafka Message Processing] Error: ", error);
-//     }
-// });
